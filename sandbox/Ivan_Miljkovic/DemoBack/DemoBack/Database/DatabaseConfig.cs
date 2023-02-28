2752a8b82243ee86060be552f32f18cb16c871ee
IvanMiljkovicPMF <108157316+IvanMiljkovicPMF@users.noreply.github.com>
2023-02-28 22:22:06 +0100

    #6 Dodat Back za Demo aplikaciju

---
+++ sandbox/Ivan_Miljkovic/DemoBack/DemoBack/Database/DatabaseConfig.cs
@@ -0,0 +1,9 @@
+﻿namespace DemoBack.Database
+{
+    public class DatabaseConfig
+    {
+        public string ConnectionString { get; set; } = null!;
+        public string DatabaseName { get; set; } = null!;
+        public string CollectionName { get; set; } = null!;
+    }
+}
