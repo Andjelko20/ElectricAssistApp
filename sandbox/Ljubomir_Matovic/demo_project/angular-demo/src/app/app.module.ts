398001a4612983adf61b7002927e6dfd4d78ab9e
Ljubomir Matovic <matovicljubomir2002@gmail.com>
2023-02-28 19:10:57 +0100

    Dodata login forma

--- sandbox/Ljubomir_Matovic/demo_project/angular-demo/src/app/app.module.ts
+++ sandbox/Ljubomir_Matovic/demo_project/angular-demo/src/app/app.module.ts
@@ -3,14 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+	FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
