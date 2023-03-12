954546fba6415dcdaa589340810afae427124898
milos.andjelkovic <milosandjelkovic20@gmail.com>
2023-03-13 00:15:53 +0100

    #16 implementacija dodavanje cioda i info window prilikom dodavanje markera

--- WattApp/client/src/app/maps/maps.component.ts
+++ WattApp/client/src/app/maps/maps.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MapInfoWindow, MapMarker } from '@angular/google-maps';
 
 @Component({
   selector: 'app-maps',
@@ -9,7 +10,13 @@ export class MapsComponent implements OnInit {
 
   constructor() { }
 
+  message = ""
+  latitude = 0.0;
+  longitude = 0.0;
+  brojac = 1;
   ngOnInit(): void {}
+
+  @ViewChild(MapInfoWindow) infoWindow: MapInfoWindow | undefined;
   display: any;
     center: google.maps.LatLngLiteral = {
         lat: 24,
@@ -21,12 +28,24 @@ export class MapsComponent implements OnInit {
   };
   markerPositions: google.maps.LatLngLiteral[] = [];
   addMarker(event: google.maps.MapMouseEvent) {
-      if (event.latLng != null) this.markerPositions.push(event.latLng.toJSON());
+    if (event.latLng != null) {
+      this.markerPositions.push(event.latLng.toJSON())
+      this.latitude = event.latLng.lat();
+      this.longitude = event.latLng.lng();
+
+    };
   }
   moveMap(event: google.maps.MapMouseEvent) {
     if (event.latLng != null) this.center = (event.latLng.toJSON());
-}
-move(event: google.maps.MapMouseEvent) {
-    if (event.latLng != null) this.display = event.latLng.toJSON();
+  }
+  move(event: google.maps.MapMouseEvent) {
+      if (event.latLng != null) this.display = event.latLng.toJSON();
+  }
+  openInfoWindow(marker: MapMarker) {
+    if (this.infoWindow != undefined) {
+      this.message = "Lokacija "+this.brojac+" koordinate su "+this.latitude+" i "+this.longitude;
+      this.brojac++;
+      this.infoWindow.open(marker);
+    }
 }
 }
