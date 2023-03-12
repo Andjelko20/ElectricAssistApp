69c5a442b128ef6ba314cf5b07047982c9536c38
milos.andjelkovic <milosandjelkovic20@gmail.com>
2023-03-12 23:36:29 +0100

    #16 popravljeni bagovi i dodata nova implementacija

--- WattApp/client/src/app/app.module.ts
+++ WattApp/client/src/app/app.module.ts
@@ -7,18 +7,23 @@ import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { MapsComponent } from './maps/maps.component';
+
+import { GoogleMapsModule } from '@angular/google-maps';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    HomeComponent
+    HomeComponent,
+    MapsComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
 	  FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    GoogleMapsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ WattApp/client/src/app/maps/maps.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-maps',
+  templateUrl: './maps.component.html',
+  styleUrls: ['./maps.component.css']
+})
+export class MapsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {}
+  display: any;
+    center: google.maps.LatLngLiteral = {
+        lat: 24,
+        lng: 12
+    };
+    zoom = 4;
+    moveMap(event: google.maps.MapMouseEvent) {
+        if (event.latLng != null) this.center = (event.latLng.toJSON());
+    }
+    move(event: google.maps.MapMouseEvent) {
+        if (event.latLng != null) this.display = event.latLng.toJSON();
+    }
+}
