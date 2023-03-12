826f43b76a3857a415383994b1b23aced237fed0
IvanMiljkovicPMF <108157316+IvanMiljkovicPMF@users.noreply.github.com>
2023-03-12 23:05:06 +0100

    #17 Promena izgleda i dodavanje podataka i nove biblioteke
    Koriscenje dummy podatak koji su slicni podacima koji ce se koristiti u aplikaciji samo na Pie Chart-u. Promena izgleda i dodavanje legende Pie Chart-a i biblioteke.

--- sandbox/Ivan_Miljkovic/client/src/app/app.module.ts
+++ sandbox/Ivan_Miljkovic/client/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import {NgChartsModule} from 'ng2-charts' 
+import {NgChartsModule} from 'ng2-charts'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BarChartComponent } from './components/bar-chart/bar-chart.component';
@@ -20,6 +20,7 @@ import { ChartsComponent } from './pages/charts/charts.component';
     BrowserModule,
     AppRoutingModule,
     NgChartsModule,
+    
     
   ],
   providers: [],
--- sandbox/Ivan_Miljkovic/client/src/app/components/pie-chart/pie-chart.component.ts
+++ sandbox/Ivan_Miljkovic/client/src/app/components/pie-chart/pie-chart.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { Chart,registerables } from 'node_modules/chart.js'
 Chart.register(...registerables)
 
+import ChartDataLabels from 'chartjs-plugin-datalabels';
+Chart.register(ChartDataLabels);
 @Component({
   selector: 'app-pie-chart',
   templateUrl: './pie-chart.component.html',
@@ -19,22 +21,86 @@ export class PieChartComponent implements OnInit {
   {
 
     const Piechart =new Chart("piechart", {
+      
       type: 'pie',
       data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        labels: ['Aerodrom', 'Bagremar', 'Erdoglija', 'Bresnica', 'Stanovo', 'Belosevac'],
         datasets: [{
-          label: '# of Votes',
-          data: [12, 19, 3, 5, 2, 3],
+          label: 'Percentage of Consumption',
+          data: [30, 20, 15, 15, 14, 6],
           borderWidth: 1
         }]
+
       },
-      options: {
-        scales: {
-          y: {
-            beginAtZero: true
-          }
+      plugins: [ChartDataLabels],
+      options:{
+        plugins: {
+          legend: {
+            position: 'top',
+          },
+          title: {
+            display: true,
+            text: 'Pie Chart'
+          },
+          datalabels: {
+            formatter: (value, ctx) => {
+                let sum = 0;
+                let dataArr = ctx.chart.data.datasets[0].data;
+                dataArr.map(data => {
+                    sum += data;
+                });
+                let percentage = (value*100 / sum).toFixed(2)+"%";
+                return percentage;
+            },
+            color: '#fff',
         }
-      }
+          
+        }
+        
+      },
+      
+      
     });
+    
   } 
-}
+// }
+// var data = [{
+//   data: [50, 55, 60, 33],
+//   labels: ["India", "China", "US", "Canada"],
+//   backgroundColor: [
+//       "#4b77a9",
+//       "#5f255f",
+//       "#d21243",
+//       "#B27200"
+//   ],
+//   borderColor: "#fff"
+// }];
+
+// var options = {
+//   tooltips: {
+//       enabled: false
+//   },
+//   plugins: {
+//       datalabels: {
+//           formatter: (value, ctx) => {
+//               let sum = 0;
+//               let dataArr = ctx.chart.data.datasets[0].data;
+//               dataArr.map(data => {
+//                   sum += data;
+//               });
+//               let percentage = (value*100 / sum).toFixed(2)+"%";
+//               return percentage;
+//           },
+//           color: '#fff',
+//       }
+//   }
+// };
+
+// var ctx = document.getElementById("pie-chart").getContext('2d');
+// var myChart = new Chart(ctx, {
+//   type: 'pie',
+//   data: {
+//       datasets: data
+//   },
+//   options: options
+// });
