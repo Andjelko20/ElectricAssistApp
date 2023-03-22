7b8d0ceb6448a10e2ce156aa4c80426a7f125855
Ljubomir Matovic <matovicljubomir2002@gmail.com>
2023-03-22 23:18:38 +0100

    #34 dodata validacija

--- WattApp/client/src/app/pages/reset-password/reset-password.component.ts
+++ WattApp/client/src/app/pages/reset-password/reset-password.component.ts
@@ -15,13 +15,39 @@ export class ResetPasswordPageComponent {
 
 	newPassword:string="";
 	confirmedPassword:string="";
+
+	public errorNewPassword:string="";
+	public errorConfirmedPassword:string="";
+
 	constructor(private route:ActivatedRoute,private router:Router,private authService:AuthService){
 		this.resetKey=this.route.snapshot.paramMap.get('id') ?? "";
 	}
+	validatePassword():boolean{
+		let input=document.getElementsByName("new_password")[0] as HTMLInputElement;
+		if(this.newPassword!=""){
+			input.classList.remove("invalid");
+			this.errorNewPassword="";
+			return true;
+		}
+		input.classList.add("invalid");
+		this.errorNewPassword="Password is required";
+		return false;
+	}
 
+	validateConfirmedPassword():boolean{
+		let input=document.getElementsByName("confirm_password")[0] as HTMLInputElement;
+		if(this.confirmedPassword!="" && this.confirmedPassword==this.newPassword){
+			input.classList.remove("invalid");
+			this.errorConfirmedPassword="";
+			return true;
+		}
+		input.classList.add("invalid");
+		this.errorConfirmedPassword="Confirmed must be that same as required and not empty";
+		return false;
+	}
 	resetPassword(){
 		// validate
-		if(this.newPassword!=this.confirmedPassword)
+		if(!this.validatePassword() || !this.validateConfirmedPassword())
 			return;
 		this.authService.resetPasswordWithResetCode(this.resetKey,this.newPassword).subscribe(
 			{
@@ -30,6 +56,7 @@ export class ResetPasswordPageComponent {
 				},
 				error:(response:HttpErrorResponse)=>{
 					this.success=false;
+					this.errorMessage="Reset key is expired or invalid";
 				}
 			}
 		);
