056d3fe6fc8b933f53512b867bc1135ca9835e41
codalic98 <dacalukic586@gmail.com>
2023-03-22 22:20:59 +0100

    Update-ovan css i html

--- WattApp/client/src/app/components/maps/maps.component.spec.ts
+++ WattApp/client/src/app/components/maps/maps.component.spec.ts
@@ -1,18 +1,34 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { MapsComponent } from './maps.component';
+<<<<<<<< HEAD:WattApp/client/src/app/components/maps/maps.component.spec.ts
+import { SidebarComponent } from './sidebar.component';
 
-describe('MapsComponent', () => {
-  let component: MapsComponent;
-  let fixture: ComponentFixture<MapsComponent>;
+describe('SidebarComponent', () => {
+  let component: SidebarComponent;
+  let fixture: ComponentFixture<SidebarComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ MapsComponent ]
+      declarations: [ SidebarComponent ]
     })
     .compileComponents();
 
-    fixture = TestBed.createComponent(MapsComponent);
+    fixture = TestBed.createComponent(SidebarComponent);
+========
+import { NavbarComponent } from './navbar.component';
+
+describe('NavbarComponent', () => {
+  let component: NavbarComponent;
+  let fixture: ComponentFixture<NavbarComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ NavbarComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(NavbarComponent);
+>>>>>>>> 30-izmene-na-stranicama-za-administriranje-korisnika:WattApp/client/src/app/components/navbar/navbar.component.spec.ts
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
