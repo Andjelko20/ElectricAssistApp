e9446a8952468054dd0d811947c9a8ec4f2dd150
Stefan Milanovic <stefanmilanovic1704@gmail.com>
2023-03-25 17:35:53 +0100

    #36 pocetna izrada servisa istorije za godinu dana

---
+++ WattApp/server/Server/Services/IHistoryService.cs
@@ -0,0 +1,9 @@
+﻿using Server.Models;
+
+namespace Server.Services
+{
+    public interface IHistoryService
+    {
+        List<DeviceEnergyUsage> GetUsageHistoryForDeviceInLastYear(int deviceId);
+    }
+}
---
+++ WattApp/server/Server/Services/Implementations/HistoryServiceImpl.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Server.Data;
+using Server.Models;
+
+namespace Server.Services.Implementations
+{
+    public class HistoryServiceImpl : IHistoryService
+    {
+        private readonly SqliteDbContext _context;
+        public HistoryServiceImpl(SqliteDbContext context)
+        {
+            _context = context;
+        }
+
+        public List<DeviceEnergyUsage> GetUsageHistoryForDeviceInLastYear(int deviceId)
+        {
+            DateTime OneYearAgo = DateTime.Now.AddYears(-1); // tip DATETIME, trenutna godina 2023. -1 = 2022.
+
+            return _context.DeviceEnergyUsages
+            .Where(u => u.DeviceId == deviceId && u.StartTime >= OneYearAgo)
+            .OrderBy(u => u.StartTime)
+            .ToList();
+        }
+
+        public Device addNewDevice(Device device)
+        {
+            var result = _context.Devices.Add(device);
+            _context.SaveChanges();
+            return device;
+        }
+    }
+}
