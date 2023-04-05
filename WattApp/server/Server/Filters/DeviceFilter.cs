1e66330614bd2d2067c02a635cb8451d64005523
tijana.petrovic <petrovic.tijanakg@gmail.com>
2023-04-05 23:55:10 +0200

    Odradjeno filtriranje za getAllDevices za prosumera

--- WattApp/server/Server/Filters/DeviceFilter.cs
+++ WattApp/server/Server/Filters/DeviceFilter.cs
@@ -11,6 +11,18 @@ namespace Server.Filters
             {
                 return devices;
             }
+            if (filter.categoryId != null)
+            {
+                devices = devices.Where(src => src.DeviceModel.DeviceType.CategoryId == filter.categoryId);
+            }
+            if (filter.typeId != null)
+            {
+                devices = devices.Where(src => src.DeviceModel.DeviceTypeId == filter.typeId);
+            }
+            if (filter.brandId != null)
+            {
+                devices = devices.Where(src => src.DeviceModel.DeviceBrandId == filter.brandId);
+            }
             if (filter.turnOn != null)
             {
                 devices = devices.Where(src => src.TurnOn == filter.turnOn);
