32c982d90bfda3b6949411ef2dec33489aeefd20
Ljubomir Matovic <matovicljubomir2002@gmail.com>
2023-04-16 16:40:17 +0200

    #76 promenjen sadrzaj app-page-control komponente i nacin realizacije paginacije zbog problema sa ucitavanjem stranice

--- WattApp/client/src/app/components/page-control/page-control.component.ts
+++ WattApp/client/src/app/components/page-control/page-control.component.ts
@@ -7,52 +7,65 @@ import { Router } from '@angular/router';
   styleUrls: ['./page-control.component.css']
 })
 export class PageControlComponent implements OnInit{
-	@Input() url!:string;
+	//@Input() url!:string;
 
-	@Input() currentPage:number=1;
-	@Output() currentPageChange:EventEmitter<number>=new EventEmitter<number>();
+	//@Input() currentPage:number=1;
+	//@Output() currentPageChange:EventEmitter<number>=new EventEmitter<number>();
 
-	@Input() itemsPerPage:number=10;
-	@Output() itemsPerPageChange:EventEmitter<number>=new EventEmitter<number>();
+	@Output() pageChange:EventEmitter<number>=new EventEmitter<number>();
 
-	@Input() totalItems:number=20;
-	@Output() totalItemsChange:EventEmitter<number>=new EventEmitter<number>();
+	//@Input() itemsPerPage:number=10;
+	//@Output() itemsPerPageChange:EventEmitter<number>=new EventEmitter<number>();
 
-	@Input() items!:any[];
-	@Output() itemsChange:EventEmitter<any[]>=new EventEmitter<any[]>();
+	//@Input() totalItems:number=20;
+	//@Output() totalItemsChange:EventEmitter<number>=new EventEmitter<number>();
+
+	//@Input() items:any[]=[];
+	//@Output() itemsChange:EventEmitter<any[]>=new EventEmitter<any[]>();
 	constructor(private router:Router){
 	}
 	ngOnInit(){
 		this.pageChanged(1);
 	}
 	pageChanged(pageNumber:number){
+		this.pageChange.emit(pageNumber);
+		//this.currentPageChange.emit(pageNumber);
+		/*
 		let url=new URL(this.url);
 		url.searchParams.set("pageNumber",pageNumber.toString());
 		url.searchParams.set("pageSize",this.itemsPerPage.toString());
 		let controller=new AbortController();
 		setTimeout(()=>{
-			alert('bbb');
 			controller.abort();
-			console.log("aborted");
 		},1000);
 		fetch(url.toString(),{headers:{"Authorization":"Bearer "+localStorage.getItem("token")},signal:controller.signal})
 		.then(res=>{
 			if(res.status==401 || res.status==403){
-				alert('aaa');
+				//alert('aaa');
 				return Promise.reject("aaa");
 			}
-			console.log('works');
-			return res.json();
+			return res.text();
 		})
-		.then(res=>{
-			console.log(res);
-			this.itemsChange.emit(res.data);
-			this.currentPageChange.emit(pageNumber);
-			this.totalItemsChange.emit(res.numberOfPages*this.itemsPerPage);
+		.then(ress=>{
+			try{
+				let res=JSON.parse(ress);
+				if(res?.data==undefined)
+					return;
+				console.log(res);
+				//this.itemsChange.emit(res?.data);
+				this.currentPageChange.emit(pageNumber);
+				this.totalItemsChange.emit(res.numberOfPages*this.itemsPerPage);
+			}
+			catch(err:any){
+				console.log(err.message);
+			}
+			
 		})
 		.catch(err=>{
+			console.log(err?.name);
 			localStorage.removeItem("token");
 			this.router.navigate(["login"]);
 		});
+		*/
 	}
 }
