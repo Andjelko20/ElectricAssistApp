e7aaf0bcde9ae0ff3826ee280f69c9d87900d4e9
Ljubomir Matovic <matovicljubomir2002@gmail.com>
2023-04-16 18:07:11 +0200

    #76 implementirana paginacija za prikaz uredjaja za dso

--- WattApp/client/src/app/components/dso/all-devices-dso/all-devices-dso.component.ts
+++ WattApp/client/src/app/components/dso/all-devices-dso/all-devices-dso.component.ts
@@ -11,8 +11,11 @@ import { Categories } from 'src/app/utilities/categories';
   styleUrls: ['./all-devices-dso.component.css']
 })
 export class AllDevicesDsoComponent implements OnInit{
-  
-  devicesList:ShowDevices[] = []
+  currentPage:number=1;
+  itemsPerPage:number=12;
+  totalItems:number=12;
+
+  devicesList:ShowDevices[] = [];
   deviceCategoryId!: number;
   idDevice!: number;
   constructor(private authService:AuthService,private deviceService:DevicesService,private route:ActivatedRoute) {}
@@ -28,7 +31,7 @@ export class AllDevicesDsoComponent implements OnInit{
 
   ngOnInit(): void {
     this.deviceCategoryId = 2;
-    this.deviceService.getDeviceProsumer(Number(this.route.snapshot.paramMap.get('id')),1,12,this.deviceCategoryId).subscribe(devices => {
+    this.deviceService.getDeviceProsumer(Number(this.route.snapshot.paramMap.get('id')),1,this.itemsPerPage,this.deviceCategoryId).subscribe(devices => {
       this.devicesList=devices.data.map((u:any)=>({
         id:u.id,
         userId: u.userId,
@@ -57,6 +60,39 @@ export class AllDevicesDsoComponent implements OnInit{
     
     );
     
+  }
+
+  pageChanged(pageNumber:number){
+	this.currentPage=pageNumber;
+	this.deviceService.getDeviceProsumer(Number(this.route.snapshot.paramMap.get('id')),pageNumber,this.itemsPerPage,this.deviceCategoryId).subscribe(devices => {
+		this.totalItems=devices.numberOfPages*this.itemsPerPage;
+		this.devicesList=devices.data.map((u:any)=>({
+		  id:u.id,
+		  userId: u.userId,
+		  deviceCategory:u.deviceCategory,
+		  deviceType: u.deviceType ,
+		  deviceBrand: u.deviceBrand ,
+		  deviceModel: u.deviceModel ,
+		  name: u.name ,
+		  energyInKwh: u.energyInKwh,
+		  standByKwh: u.standByKwh,
+		  visibility: u.visibility,
+		  controlability: u.controlability,
+		  turnOn: u.turnOn,
+	  
+	  })as ShowDevices)
+	  
+	  }, (error: { status: number; }) => {
+	  
+	  if (error.status === 404) {
+	  
+		this.devicesList=[]
+	  
+		console.log('Devices not found in database');
+	  
+	  }}
+	  
+	  );
   }
 
   onSelectedCategory(event:any){
