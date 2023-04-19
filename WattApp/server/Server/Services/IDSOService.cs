b7cd15963689131f33506fcce2bf0ea1f53d9c03
Stefan Milanovic <stefanmilanovic1704@gmail.com>
2023-04-19 21:11:27 +0200

    #72 - servis i api koji vraća naselja za prosleđen cityId

--- WattApp/server/Server/Services/IDSOService.cs
+++ WattApp/server/Server/Services/IDSOService.cs
@@ -1,7 +1,10 @@
-﻿namespace Server.Services
+﻿using Server.DTOs;
+
+namespace Server.Services
 {
     public interface IDSOService
     {
         public long GetCityId(string cityName);
+        public List<SettlementDTO> GetSettlements(long cityId);
     }
 }
