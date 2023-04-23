965b0ce27d0c177f2eb1fe79ce645154dd89f129
IvanMiljkovicPMF <ivannmiljkovic00@gmail.com>
2023-04-24 01:04:41 +0200

    #82 popravljen delete block, unblock, namesteni satovi na dashboardu
    Popravljen delete kada se izbrise korisnik izbacen je refresh page samo se ponovo ucita sa apija svi useri, block/unblock popravljen eventlistener je pamtio prethodni id kada se klikne dugme i onda nakon promene drugog id promeni se i prethodni. dodat je remove listener. Kada se predje 1000 kWh onda se menja u 1 MWh , a kada se predje 1000 MWh onda se menja u 1 GWh. Dodata legenda za svaki sat

--- WattApp/client/src/app/components/admin/admin-dso/admin-dso.component.ts
+++ WattApp/client/src/app/components/admin/admin-dso/admin-dso.component.ts
@@ -14,6 +14,9 @@ export class AdminDsoComponent {
 	totalItems:number=10;
   
   showUsers:ShowUsers[]=[];
+  onBlockClick!: (this: HTMLElement, ev: MouseEvent) => any;
+  onUnblockClick!: (this: HTMLElement, ev: MouseEvent) => any;
+  
   constructor(private router:Router,private usersService:AuthService,
     private route:ActivatedRoute) { }
 
@@ -55,39 +58,38 @@ export class AdminDsoComponent {
 		   });
 	}
 
-  confirmBlock(id: number) {
+  blockUser(id: number) {
     const block = document.getElementById('block-popup');
     if (block != null) {
-      block.addEventListener('click', () => {
-        this.blockUser(id);
-      });
+      block.removeEventListener('click', this.onBlockClick); // remove previous event listener
+      this.onBlockClick = () => { // create new event listener function
+        this.usersService.blockUser(id).subscribe(() => {
+          
+          const userIndex = this.showUsers.findIndex(user => user.id === id);
+          this.showUsers[userIndex].block = true;
+        });
+        block.removeEventListener('click', this.onBlockClick); // remove event listener after execution
+      };
+      block.addEventListener('click', this.onBlockClick); // add new event listener
     }
   }
   
-  blockUser(id: number) {
-    this.usersService.blockUser(id).subscribe(() => {
-      console.log(id + "a");
-      const userIndex = this.showUsers.findIndex(user => user.id === id);
-      this.showUsers[userIndex].block = true;
-    });
-  }
-  confirmUnblock(id: number) {
+  unblockUser(id: number) {
     const unblock = document.getElementById('unblock-popup');
     if (unblock != null) {
-      unblock.addEventListener('click', () => {
-        this.unblockUser(id);
-      });
+      unblock.removeEventListener('click', this.onUnblockClick); // remove previous event listener
+      this.onUnblockClick = () => { // create new event listener function
+        this.usersService.unblockUser(id).subscribe(() => {
+  
+          const userIndex = this.showUsers.findIndex(user => user.id === id);
+          this.showUsers[userIndex].block = false;
+        });
+        unblock.removeEventListener('click', this.onUnblockClick); // remove event listener after execution
+      };
+      unblock.addEventListener('click', this.onUnblockClick); // add new event listener
     }
   }
-  unblockUser(id: number) {
-    
-    this.usersService.unblockUser(id).subscribe(() => {
-      console.log(id + "f");
-      const userIndex1 = this.showUsers.findIndex(user => user.id === id);
-      this.showUsers[userIndex1].block = false;
-    });
-  }
-
+  
 
   getUsers()
   {
@@ -96,8 +98,6 @@ export class AdminDsoComponent {
 
   delete(id:number)
   {
-    
-      
    
     const deletePopup= document.getElementById('delete-admin-popup');
     
@@ -107,7 +107,21 @@ export class AdminDsoComponent {
       this.usersService.delete(id)
       .subscribe(()=>{
           this.router.navigate(['dashboard']);
-          location.reload();
+          this.usersService.getAllUsers(1).subscribe(users => {
+            this.totalItems=users.numberOfPages*this.itemsPerPage;
+              this.showUsers=users.data.map((u:any)=>({
+                id: u.id,
+                name: u.name,
+                username: u.username,
+                block: u.blocked,
+                email: u.email,
+                role: u.role,
+                settlement:u.settlement,
+                city:u.city,
+                address:u.address,
+                country:u.country
+              } as ShowUsers));
+             });
       });
      });
     }
