830ebd603d1d4e940d3d0da9dd7b6017c5762f77
codalic98 <dacalukic586@gmail.com>
2023-04-25 22:20:59 +0200

    popravljen footer, zavrsen dropdown za jednog uredjaja i popravljen css za sve uredjaje za visinu #86

--- WattApp/client/src/app/pages/prosumer/prosumer-device-page/prosumer-device-page.component.ts
+++ WattApp/client/src/app/pages/prosumer/prosumer-device-page/prosumer-device-page.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {  Component, ElementRef, ViewChildren, QueryList, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-prosumer-device-page',
@@ -6,5 +6,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./prosumer-device-page.component.css']
 })
 export class ProsumerDevicePageComponent {
+  @ViewChildren('collapsibleButton') collapsibleButtons!: QueryList<ElementRef>;
+
+  constructor(private elementRef: ElementRef) {}
+
+  ngAfterViewInit() {
+    this.collapsibleButtons.forEach(button => {
+      button.nativeElement.addEventListener('click', () => {
+        button.nativeElement.classList.toggle('active');
+        const content = button.nativeElement.nextElementSibling;
+        if (content.style.display === 'block') {
+          content.style.display = 'none';
+        } else {
+          content.style.display = 'block';
+        }
+      });
+    });
+  }
 
 }
