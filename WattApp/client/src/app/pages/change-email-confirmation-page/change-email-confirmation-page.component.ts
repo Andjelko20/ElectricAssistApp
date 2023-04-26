282fc474ad331faa757b3e3e79d427a1813bf8f1
tijana.petrovic <petrovic.tijanakg@gmail.com>
2023-04-26 16:30:30 +0200

    Kreirana strana change-email-confirmation

--- WattApp/client/src/app/pages/change-email-confirmation-page/change-email-confirmation-page.component.ts
+++ WattApp/client/src/app/pages/change-email-confirmation-page/change-email-confirmation-page.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-change-email-confirmation-page',
@@ -6,5 +8,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./change-email-confirmation-page.component.css']
 })
 export class ChangeEmailConfirmationPageComponent {
+  isConfirmed : boolean = false;
+  constructor(private http : HttpClient, private route : ActivatedRoute, private router : Router){
 
+  }
+
+  goToLogin(){
+    this.router.navigate(['login']);
+  }
+
+  ngOnInit(){
+    this.route.queryParams.subscribe(params => {
+      const key = params['key'];
+      console.log(key);
+      this.http.post<ConfirmEmailResponseDTO>(`https://localhost:7146/api/Users/changeEmailConfirmation/${key}`, undefined).subscribe((response) => {
+        console.log(response);
+        if(response && response.isConfirmed){
+          this.isConfirmed = true;
+        }
+      });
+    });
+  }
+}
+
+interface ConfirmEmailResponseDTO{
+  isConfirmed : boolean;
+  error : string;
 }
