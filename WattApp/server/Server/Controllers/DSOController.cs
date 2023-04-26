f023969a75ca5d55efdf5baf24c7340f801321ad
IvanMiljkovicPMF <ivannmiljkovic00@gmail.com>
2023-04-26 22:10:58 +0200

    izbrisana suvisna funkcija

--- WattApp/server/Server/Controllers/DSOController.cs
+++ WattApp/server/Server/Controllers/DSOController.cs
@@ -112,57 +112,6 @@ namespace Server.Controllers
             return BadRequest("Input parameters are empty.");
         }
 
-        /// <summary>
-        /// 1.) Get double consumptio/production for cityId - today or this month || 2.) Get cityId
-        /// </summary>
-        [HttpGet]
-        [Route("City/")]
-        //[Authorize(Roles = "dispecer, prosumer, guest")]
-        public async Task<IActionResult> GetCity([FromQuery] long deviceCategoryId, [FromQuery] long todayByHourCityId, long todayCityId, long thisMonthCityId, long thisYearCityId, string cityName="null")
-        {
-            if(!cityName.Equals("null"))
-            {
-                var cityId = dsoService.GetCityId(cityName);
-
-                if (cityId == -1)
-                    return NotFound(new { message = "City with name: " + cityName.ToString() + " does not exist." });
-
-                return Ok(cityId);
-            }
-
-            if (!_sqliteDb.DeviceCategories.Any(dc => dc.Id == deviceCategoryId))
-                return NotFound(new { message = "Device category with ID: " + deviceCategoryId.ToString() + " doesnt exist"});        
-
-            if (todayCityId!=0)
-            {
-                if (!_sqliteDb.Cities.Any(c => c.Id == todayCityId))
-                    return NotFound(new { message = "City with ID: " + todayCityId.ToString() + " doesnt exist" });
-                
-                return Ok(dsoService.GetCityConsumptionForToday(todayCityId, deviceCategoryId));
-            } 
-            else if(thisMonthCityId!=0)
-            {
-                if (!_sqliteDb.Cities.Any(c => c.Id == thisMonthCityId))
-                    return NotFound(new { message = "City with ID: " + thisMonthCityId.ToString() + " doesnt exist" });
-
-                return Ok(dsoService.GetUsageHistoryForDeviceInThisMonth(thisMonthCityId, deviceCategoryId));
-            }
-            else if(thisYearCityId!=0)
-            {
-                if (!_sqliteDb.Cities.Any(c => c.Id == thisYearCityId))
-                    return NotFound(new { message = "City with ID: " + thisYearCityId.ToString() + " doesnt exist" });
-
-                return Ok(dsoService.GetUsageHistoryForDeviceInThisYear(thisYearCityId, deviceCategoryId));
-            }
-            else if (todayByHourCityId != 0)
-            {
-                if (!_sqliteDb.Cities.Any(c => c.Id == todayByHourCityId))
-                    return NotFound(new { message = "City with ID: " + todayByHourCityId.ToString() + " doesnt exist" });
-
-                return Ok(dsoService.CalculateEnergyUsageForTodayInCity(todayByHourCityId, deviceCategoryId));
-            }
-
-            return BadRequest("Input parameters are empty.");
-        }
+       
     }
 }
