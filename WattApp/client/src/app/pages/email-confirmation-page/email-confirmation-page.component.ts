47f32ecfb9ea61d0d4e55f19081024001046a0d5
tijana.petrovic <petrovic.tijanakg@gmail.com>
2023-04-30 22:27:38 +0200

    Problem na frontu -> pri ponovnom pokretanju, ne radi

--- WattApp/client/src/app/pages/email-confirmation-page/email-confirmation-page.component.ts
+++ WattApp/client/src/app/pages/email-confirmation-page/email-confirmation-page.component.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
 import { environment } from 'src/environments/environment';
 
 @Component({
@@ -8,39 +9,51 @@ import { environment } from 'src/environments/environment';
   templateUrl: './email-confirmation-page.component.html',
   styleUrls: ['./email-confirmation-page.component.css']
 })
-export class EmailConfirmationPageComponent implements OnInit{
-  isConfirmed : boolean | null = null;
-  message : string | null = null;
-  constructor(private http : HttpClient, private route : ActivatedRoute, private router : Router){
+export class EmailConfirmationPageComponent implements OnInit, OnDestroy {
+  isConfirmed: boolean | null = null;
+  message: string | null = null;
+  private routeSubscription: Subscription | null = null;
 
-  }
+  constructor(private http: HttpClient, private route: ActivatedRoute, private router: Router) {}
 
-  goToLogin(){
+  goToLogin() {
     this.router.navigate(['login']);
   }
 
-  ngOnInit(){
-    this.route.queryParams.subscribe(params => {
+  ngOnInit() {
+    console.log("ngOnInit called");
+    this.routeSubscription?.unsubscribe();
+    this.subscribeToRoute();
+  }
+
+  private subscribeToRoute() {
+    this.routeSubscription = this.route.queryParams.subscribe(params => {
       const key = encodeURIComponent(params['key']);
       console.log(key);
-      this.http.post<ConfirmEmailResponseDTO>(`${environment.serverUrl}/api/Users/emailConfirmation/${key}`, null).subscribe((response) => {
-        console.log(response);
-        console.log(response.isConfirmed);
-        console.log(response.error);
-        if(response.isConfirmed){
-          this.isConfirmed = true;
-        }
-        else{
-          this.isConfirmed = false;
-          this.message = response.error;
-        }
-      });
+      this.http.post<ConfirmEmailResponseDTO>(`${environment.serverUrl}/api/Users/emailConfirmation/${key}`, null)
+        .subscribe((response) => {
+          console.log(response);
+          console.log(response.isConfirmed);
+          console.log(response.error);
+          if (response.isConfirmed) {
+            this.isConfirmed = true;
+          } else {
+            this.isConfirmed = false;
+            this.message = response.error;
+          }
+        });
     });
   }
 
+  ngOnDestroy(): void {
+    console.log("ngOnDestroy called");
+    if (this.routeSubscription) {
+      this.routeSubscription.unsubscribe();
+    }
+  }
 }
 
-interface ConfirmEmailResponseDTO{
-  isConfirmed : boolean;
-  error : string;
+interface ConfirmEmailResponseDTO {
+  isConfirmed: boolean;
+  error: string;
 }
