fa841576073add05a216f602e7aae96519c75487
IvanMiljkovicPMF <ivannmiljkovic00@gmail.com>
2023-05-03 17:43:22 +0200

    #87 Popravljen resposive za one porosumer stranu i lista uredjaja. Dodata strana prediction za DSO

--- WattApp/client/src/app/components/dso/meter-prosumer/today-consumption/today-consumption.component.ts
+++ WattApp/client/src/app/components/dso/meter-prosumer/today-consumption/today-consumption.component.ts
@@ -13,7 +13,10 @@ export class TodayConsumptionComponent implements OnInit{
   valuekWh!: any;
   valueMWh!: any;
   valueGWh!: any;
-
+  min: number = 0;
+  maxkwh: number = 60000;
+  maxmwh: number = this.maxkwh*0.001;
+  maxgwh: number = this.maxmwh*0.001;
   constructor(private historyService:HistoryPredictionService,private route:ActivatedRoute){}
   ngOnInit(): void {
     this.historyService.historyDayUser(Number(this.route.snapshot.paramMap.get('id')),2).subscribe(number=>{
@@ -28,22 +31,54 @@ export class TodayConsumptionComponent implements OnInit{
       }
     })
   }
-  min: number = 0;
-  max: number = 1000;
-  markerConfig = {
+
+  markerConfigKWh = {
     "0": { color: '#57A75B', size: 8, label: '0', type: 'line'},
-    "150": { color: '#57A75B', size: 4, type: 'line'},
-    "300": { color: '#57A75B', size: 8, label: '300', type: 'line'},
-    "400": { color: '#F9D435', size: 4, type: 'line'},
-    "500": { color: '#F9D435', size: 8, label: '500', type: 'line'},
-    "600": { color: '#F69E0B', size: 4, type: 'line'},
-    "700": { color: '#F69E0B', size: 8, label: '700', type: 'line'},
-    "850": { color: '#E0453A', size: 4, type: 'line'},
-    "1000": { color: '#E0453A', size: 8, label: '1000', type: 'line'},
+    "7500": { color: '#57A75B', size: 4, type: 'line'},
+    "15000": { color: '#57A75B', size: 8, label: '15000', type: 'line'},
+    "22500": { color: '#F9D435', size: 4, type: 'line'},
+    "30000": { color: '#F9D435', size: 8, label: '30000', type: 'line'},
+    "37500": { color: '#F69E0B', size: 4, type: 'line'},
+    "45000": { color: '#F69E0B', size: 8, label: '45000', type: 'line'},
+    "52500": { color: '#E0453A', size: 4, type: 'line'},
+    "60000": { color: '#E0453A', size: 8, label: '60000', type: 'line'},
   }
-  thresholdConfig = {
+  thresholdConfigKWh = {
     '0': { color: 'green', "bgOpacity": 0.2 },
-    '400': { color: 'orange', "bgOpacity": 0.2 },
-    '750': { color: 'red', "bgOpacity": 0.2 }
+    '22223': { color: 'blue', "bgOpacity": 0.2 },
+    '44445': { color: 'red', "bgOpacity": 0.2 }
+  };
+
+  markerConfigMWh = {
+    "0": { color: '#57A75B', size: 8, label: '0', type: 'line'},
+    "7.5": { color: '#57A75B', size: 4, type: 'line'},
+    "15": { color: '#57A75B', size: 8, label: '15', type: 'line'},
+    "22.5": { color: '#F9D435', size: 4, type: 'line'},
+    "30": { color: '#F9D435', size: 8, label: '30', type: 'line'},
+    "37.5": { color: '#F69E0B', size: 4, type: 'line'},
+    "45": { color: '#F69E0B', size: 8, label: '45', type: 'line'},
+    "52.5": { color: '#E0453A', size: 4, type: 'line'},
+    "60": { color: '#E0453A', size: 8, label: '60', type: 'line'},
+  }
+  thresholdConfigMWh = {
+    '0': { color: 'green', "bgOpacity": 0.2 },
+    '22.223': { color: 'blue', "bgOpacity": 0.2 },
+    '44.445': { color: 'red', "bgOpacity": 0.2 }
+  };
+  markerConfigGWh = {
+    "0": { color: '#57A75B', size: 8, label: '0', type: 'line'},
+    "0.007": { color: '#57A75B', size: 4, type: 'line'},
+    "0.015": { color: '#57A75B', size: 8, label: '0.015', type: 'line'},
+    "0.023": { color: '#F9D435', size: 4, type: 'line'},
+    "0.03": { color: '#F9D435', size: 8, label: '0.03', type: 'line'},
+    "0.038": { color: '#F69E0B', size: 4, type: 'line'},
+    "0.045": { color: '#F69E0B', size: 8, label: '0.045', type: 'line'},
+    "0.053": { color: '#E0453A', size: 4, type: 'line'},
+    "0.06": { color: '#E0453A', size: 8, label: '0.06', type: 'line'},
+  }
+  thresholdConfigGWh = {
+    '0': { color: 'green', "bgOpacity": 0.2 },
+    '0.02': { color: 'blue', "bgOpacity": 0.2 },
+    '0.04': { color: 'red', "bgOpacity": 0.2 }
   };
 }
--- WattApp/client/src/app/pages/dso/dso-prediction-page/dso-prediction-page.component.ts
+++ WattApp/client/src/app/pages/dso/dso-prediction-page/dso-prediction-page.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-dso-prediction-page',
   templateUrl: './dso-prediction-page.component.html',
   styleUrls: ['./dso-prediction-page.component.css']
 })
-export class DsoPredictionPageComponent {
+export class DsoPredictionPageComponent implements OnInit{
+  
+  
+  currentTime!: Date;
+  
+  ngOnInit(): void {
+    this.updateTime();
+    setInterval(() => {
+      this.updateTime();
+    }, 1000);
+  }
+  updateTime() {
+    this.currentTime = new Date();
+  }
 
+  
 }
