2852fe0039fe8780fc6eb67fab58f1b9a4fdc1d3
Stefan Milanovic <stefanmilanovic1704@gmail.com>
2023-05-03 04:58:11 +0200

    #88 - servis i api za predikciju potrošnje/proizvodnje jednog grada, u narednih 7 dana

--- WattApp/server/Server/Services/Implementations/PredictionServiceImpl.cs
+++ WattApp/server/Server/Services/Implementations/PredictionServiceImpl.cs
@@ -120,5 +120,80 @@ namespace Server.Services.Implementations
                 return energyUsages;
             }
         }
+
+        public List<DailyEnergyConsumptionPastMonth> CityPredictionForTheNextWeek(long cityId, long deviceCategoryId)
+        {
+            using (var _connection = _context.Database.GetDbConnection())
+            {
+                _connection.Open();
+                var command = _connection.CreateCommand();
+                command.CommandText = @"
+                                        SELECT DATE(deu.StartTime) AS YYMMDD, SUM(CAST((strftime('%s', deu.EndTime) - strftime('%s', deu.StartTime)) / 3600.0 AS REAL) * dm.EnergyKwh) AS EnergyUsageKwh
+                                        FROM DeviceEnergyUsages deu JOIN Devices d ON deu.DeviceId=d.Id
+							            JOIN DeviceModels dm ON d.DeviceModelId=dm.Id
+							            JOIN DeviceTypes dt ON dm.DeviceTypeId=dt.Id AND dt.CategoryId=@deviceCategoryId
+							            JOIN Users u ON d.UserId=u.Id
+                                        JOIN Settlements s ON s.Id = u.SettlementId AND s.CityId = @cityId
+                                        WHERE deu.StartTime >= date('now', '+1 days') 
+                                            AND (deu.EndTime <= date('now', '+8 days') OR deu.EndTime IS NULL)
+                                        GROUP BY DATE(deu.StartTime)";
+
+                command.Parameters.Add(new SqliteParameter("@deviceCategoryId", deviceCategoryId));
+                command.Parameters.Add(new SqliteParameter("@cityId", cityId));
+
+                var energyUsages = new List<DailyEnergyConsumptionPastMonth>();
+
+                using (var reader = command.ExecuteReader())
+                {
+                    if (reader.HasRows)
+                    {
+                        while (reader.Read())
+                        {
+                            DateTime date = DateTime.ParseExact(reader["YYMMDD"].ToString(), "yyyy-MM-dd", CultureInfo.InvariantCulture);
+
+                            var day = date.Day;
+                            var month = date.ToString("MMMM");
+                            var year = date.Year;
+                            var energyUsage = double.Parse(reader["EnergyUsageKwh"].ToString());
+
+                            var dailyEnergyUsage = new DailyEnergyConsumptionPastMonth
+                            {
+                                Day = day,
+                                Month = month,
+                                Year = year,
+                                EnergyUsageResult = Math.Round(energyUsage, 2)
+                            };
+
+                            energyUsages.Add(dailyEnergyUsage);
+                        }
+                    }
+                    else
+                    {
+                        var startDate = DateTime.Now.Date.AddDays(1);
+                        var endDate = startDate.AddDays(7);
+
+                        for (var date = startDate; date < endDate; date = date.AddDays(1))
+                        {
+                            var dailyEnergyUsage = new DailyEnergyConsumptionPastMonth
+                            {
+                                Day = date.Day,
+                                Month = date.ToString("MMMM"),
+                                Year = date.Year,
+                                EnergyUsageResult = 0
+                            };
+
+                            energyUsages.Add(dailyEnergyUsage);
+                        }
+                    }
+                }
+
+                return energyUsages;
+            }
+        }
+
+        public List<DailyEnergyConsumptionPastMonth> SettlementPredictionForTheNextWeek(long settlementId, long deviceCategoryId)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
