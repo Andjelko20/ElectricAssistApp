1d96f28b837e88f964f5c0368d722ab279512625
IvanMiljkovicPMF <ivannmiljkovic00@gmail.com>
2023-05-04 05:25:57 +0200

    kompresovane slike dizajn popupa kod prosumera popravljen

--- WattApp/client/src/app/components/prosumers/devices/update-device/update-device.component.ts
+++ WattApp/client/src/app/components/prosumers/devices/update-device/update-device.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, TemplateRef, ViewChild } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import {  ShowDevices, updateDevices } from 'src/app/models/devices.model';
 import { DevicesService } from 'src/app/services/devices.service';
 import { JwtToken } from 'src/app/utilities/jwt-token';
@@ -13,11 +14,14 @@ import { JwtToken } from 'src/app/utilities/jwt-token';
 })
 export class UpdateDeviceComponent implements OnInit{
 
-  
+  @ViewChild('modalContent') modalContent!: TemplateRef<any>;
+  @ViewChild('modalContent1') modalContent1!: TemplateRef<any>;
   onClickVisibility!: (this: HTMLElement, ev: MouseEvent) => any;
   offClickVisibility!: (this: HTMLElement, ev: MouseEvent) => any;
   onClickControlability!: (this: HTMLElement, ev: MouseEvent) => any;
   offClickControlability!: (this: HTMLElement, ev: MouseEvent) => any;
+  body: string = ''; 
+  btnAction:string='';
   myForm: FormGroup;
   updateDevice:updateDevices={
     id: 0,
@@ -35,7 +39,8 @@ export class UpdateDeviceComponent implements OnInit{
   devices:ShowDevices[] = [];
   idProsumer?:number;
   idDevice?:number;
-  constructor(private devicesService:DevicesService,private router:Router,private route:ActivatedRoute,private formBuilder: FormBuilder) {
+  constructor(private devicesService:DevicesService,private router:Router,private route:ActivatedRoute
+    ,private formBuilder: FormBuilder,private modalService: NgbModal) {
     this.myForm = this.formBuilder.group({
       nameform1: ['', Validators.required],
        nameform2: ['', Validators.required],
@@ -82,22 +87,42 @@ export class UpdateDeviceComponent implements OnInit{
     });
   }
 
-  controlabilityOnOff(){
-    const controlabilityOnPopup= document.getElementById('controlability-on-popup');
-    const controlabilityOffPopup= document.getElementById('controlability-off-popup');
-    this.confirmControlability=false;
-    if(controlabilityOnPopup!=null)
+  controlabilityOn(){
+    if( this.updateDevice.visibility===false)
     {
-      controlabilityOnPopup.removeEventListener('click', this.onClickControlability);
-      this.onClickControlability=()=> {
-          this.updateDevice.controlability=true
-          this.confirmControlability=true;
-          controlabilityOnPopup.removeEventListener('click',this.onClickControlability);
-     };
-     controlabilityOnPopup.addEventListener('click',this.onClickControlability);
+      this.modalService.open(this.modalContent1);
     }
+    else
+    {
+
+      
+      this.modalService.open(this.modalContent);
+      const controlabilityOnPopup= document.getElementById('popup');
+    
+      this.confirmControlability=false;
+      if(controlabilityOnPopup!=null)
+      {this.body="Do you want to turn on controlability?"
+      this.btnAction="Turn On";
+        controlabilityOnPopup.removeEventListener('click', this.onClickControlability);
+        this.onClickControlability=()=> {
+            this.updateDevice.controlability=true
+            this.confirmControlability=true;
+            controlabilityOnPopup.removeEventListener('click',this.onClickControlability);
+      };
+      controlabilityOnPopup.addEventListener('click',this.onClickControlability);
+      }
+  }
+    
+  }
+  controlabilityOff(){
+    this.modalService.open(this.modalContent);
+    const controlabilityOffPopup= document.getElementById('popup');
+    
+    this.confirmControlability=false;
     if(controlabilityOffPopup!=null)
     {
+      this.body="Do you want to turn off controlability?"
+      this.btnAction="Turn Off";
       controlabilityOffPopup.removeEventListener('click', this.offClickControlability);
       this.offClickControlability=()=> {
           this.updateDevice.controlability=false;
@@ -106,46 +131,56 @@ export class UpdateDeviceComponent implements OnInit{
       };
       controlabilityOffPopup.addEventListener('click',this.offClickControlability);
     }
-    
   }
-  visibilityOnOff()
+
+  visibilityOn()
   {
     
-    const visibilityOnPopup= document.getElementById('visibility-on-popup');
-    const visibilityOffPopup= document.getElementById('visibility-off-popup');
+    this.modalService.open(this.modalContent);
+    const visibilityOnPopup= document.getElementById('popup');
+ 
     this.confirmVisibility=false;
     if(visibilityOnPopup!=null)
-    {
+    {this.body="Do you want to turn on visibility?"
+    this.btnAction="Turn On";
       visibilityOnPopup.removeEventListener('click', this.onClickVisibility);
       this.onClickVisibility=()=> {
         
           this.updateDevice.visibility=true
           this.confirmVisibility=true;
-          // console.log( this.updateDevice.visibility);
+
         
           visibilityOnPopup.removeEventListener('click',this.onClickVisibility);
      };
      visibilityOnPopup.addEventListener('click',this.onClickVisibility);
     
     }
+    
+    
+    
+  }
+  visibilityOff()
+  {
+    this.modalService.open(this.modalContent);
+    
+    const visibilityOffPopup= document.getElementById('popup');
+    this.confirmVisibility=false;
     if(visibilityOffPopup!=null)
     {
+      this.body="Do you want to turn off visibility?"
+      this.btnAction="Turn Off";
       visibilityOffPopup.removeEventListener('click', this.offClickVisibility);
       this.offClickVisibility=()=> {
         
           this.updateDevice.visibility=false;
           this.updateDevice.controlability=false;
           this.confirmVisibility=true;
-          // console.log( this.updateDevice.visibility);
         
           visibilityOffPopup.removeEventListener('click',this.offClickVisibility);
      };
      visibilityOffPopup.addEventListener('click',this.offClickVisibility);
     
     }
-    
-    
   }
-
 
 }
