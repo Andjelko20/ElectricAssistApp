55d3f1caca65de111aedec76a923ea0fae262a68
tijana.petrovic <petrovic.tijanakg@gmail.com>
2023-05-07 22:20:56 +0200

    Priprema za kreiranje zajednickog api-a za prosumere i njihovu trenutnu potrosnju/proizvodnju

---
+++ WattApp/server/Server/DTOs/ProsumerForDSOResponseDTO.cs
@@ -0,0 +1,11 @@
+﻿using Server.DTOs.Responses;
+
+namespace Server.DTOs
+{
+    public class ProsumerForDSOResponseDTO
+    {
+        public UserDetailsDTO userDetailsDTO { get; set; }
+        public double CurrentConsumption { get; set; } = 0.0;
+        public double CurrentProduction { get; set; } = 0.0;
+    }
+}
